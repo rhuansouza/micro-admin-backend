78c6ea0e977bc040ee7fb7f5714d6b82c95941ef
Rhuan Costanzo <rhuan@cooxupe.com>
2023-01-30 16:50:06 -0300

    feat: rabbitMQ

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Mongoose } from 'mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriaSchema } from './interfaces/categorias/categoria.schema';
+import { JogadorSchema } from './interfaces/jogadores/jogador.schema';
+//import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:admin@cluster0.yhepsrk.mongodb.net/microadmindb?retryWrites=true&w=majority',
+      {
+        //useNewUrlParser: true,
+        // useCreateIndex: true,
+        useUnifiedTopology: true,
+        // useFindAndModify: false,
+      },
+    ),
+    MongooseModule.forFeature([
+      { name: 'Categoria', schema: CategoriaSchema },
+      { name: 'Jogador', schema: JogadorSchema },
+    ]),
+    // CategoriasModule,
+    // JogadoresModule,
+    //ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+  Logger,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Categoria } from '../src/interfaces/categorias/categoria.interface';
+import { Model } from 'mongoose';
+import { Jogador } from './interfaces/jogadores/jogador.interface';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectModel('Categoria')
+    private readonly categoriaModel: Model<Categoria>,
+
+    @InjectModel('Jogador')
+    private readonly jogadorModel: Model<Jogador>,
+  ) {}
+
+  private logger = new Logger(AppService.name);
+
+  async criarCategoria(categoria: Categoria): Promise<Categoria> {
+    try {
+      const categoriaCriada = new this.categoriaModel(categoria);
+      return await categoriaCriada.save();
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`);
+      throw new RpcException(error.message);
+    }
+  }
+
+  async consultarTodasCategorias(): Promise<Categoria[]> {
+    try {
+      return await this.categoriaModel.find().exec();
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`);
+      throw new RpcException(error.message);
+    }
+  }
+
+  async consultarCategoriaPeloId(_id: string): Promise<Categoria> {
+    try {
+      return await this.categoriaModel.findOne({ _id }).exec();
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`);
+      throw new RpcException(error.message);
+    }
+  }
+
+  async atualizarCategoria(_id: string, categoria: Categoria): Promise<void> {
+    try {
+      await this.categoriaModel
+        .findOneAndUpdate({ _id }, { $set: categoria })
+        .exec();
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`);
+      throw new RpcException(error.message);
+    }
   }
 }
